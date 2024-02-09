f0286a869f8c2a8dc8091a31879a11bf5aca2c0c
Haider Mahmood <haider.slsc@gmail.com>
2024-02-10 03:02:37 +0500

    Completed the Feedback Submission Form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FeedbackController;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/feedback', [FeedbackController::class, 'view'])->name('feedback.view');
+Route::post('/feedback', [FeedbackController::class, 'create'])->name('feedback.create');
