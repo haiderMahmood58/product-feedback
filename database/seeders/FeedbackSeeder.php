e1db2411632756f66bd9d6fd37011996498207e0
Haider Mahmood <haider.slsc@gmail.com>
2024-02-10 22:58:25 +0500

    TS:: write tests and add seed files

--- database/seeders/FeedbackSeeder.php
+++ database/seeders/FeedbackSeeder.php
@@ -11,39 +11,6 @@ class FeedbackSeeder extends Seeder
 {
     public function run(): void
     {
-
         Feedback::factory()->count(10)->create();
-
-        // $randomUser = User::inRandomOrder()->first();
-
-        // if ($randomUser === null) {
-        //     $this->command->error('Failed to fetch a random user. Skipping feedback seeding.');
-        //     return;
-        // }
-
-        // $feedbackData = [
-        //     [
-        //         'user_id' => $randomUser->id,
-        //         'title' => 'Bug Report',
-        //         'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        //         'category' => 'bug',
-        //     ],
-        //     [
-        //         'user_id' => $randomUser->id,
-        //         'title' => 'Feature Request',
-        //         'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        //         'category' => 'feature',
-        //     ],
-        //     [
-        //         'user_id' => $randomUser->id,
-        //         'title' => 'Improvement Request',
-        //         'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        //         'category' => 'improvement',
-        //     ]
-        // ];
-
-        // foreach ($feedbackData as $data) {
-        //     Feedback::create($data);
-        // }
     }
 }
